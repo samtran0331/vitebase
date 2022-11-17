77da2a9b991e7dbcf47a46042200a55ca8206df3
Sam Tran <samtran0331@gmail.com>
2022-11-17 15:36:24 -0500

    Initial setup and test

---
+++ src/pages/desktop/calendar/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+	This is the index.vue page in the calendar folder godammit!
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
