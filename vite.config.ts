829ebc1de555281d68aff886ef46ea11f611823b
Sam Tran <samtran0331@gmail.com>
2022-11-17 16:53:37 -0500

    Revert "Merge pull request #1 from samtran0331/split-vite-config"
    This reverts commit 19b57f95ae0e65a6dfda442557cc67420dcf2b2d, reversing
    changes made to 77da2a9b991e7dbcf47a46042200a55ca8206df3.

--- vite.config.ts
+++ vite.config.ts
@@ -15,267 +15,142 @@ import LinkAttributes from 'markdown-it-link-attributes'
 import Unocss from 'unocss/vite'
 import Shiki from 'markdown-it-shiki'
 
-export default defineConfig(({ command, mode }) => {
-  if (command === 'serve') {
-    // This is the development path
-    return {
-      resolve: {
-        alias: {
-          '~/': `${path.resolve(__dirname, 'src')}/`,
-        },
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
 
-      plugins: [
-        Preview(),
+  plugins: [
+    Preview(),
 
-        Vue({
-          include: [/\.vue$/, /\.md$/],
-          reactivityTransform: true,
-        }),
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
+    }),
 
-        Pages({
-          extensions: ['vue', 'md'],
-        }),
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages({
+      extensions: ['vue', 'md'],
+    }),
 
-        Layouts(),
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts(),
 
-        AutoImport({
-          imports: [
-            'vue',
-            'vue-router',
-            'vue-i18n',
-            'vue/macros',
-            '@vueuse/head',
-            '@vueuse/core',
-          ],
-          dts: 'src/auto-imports.d.ts',
-          dirs: [
-            'src/composables',
-            'src/store',
-          ],
-          vueTemplate: true,
-        }),
-
-        Components({
-          // allow auto load markdown components under `./src/components/`
-          extensions: ['vue', 'md'],
-          // allow auto import and register components used in markdown
-          include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-          dts: 'src/components.d.ts',
-        }),
-
-        // see unocss.config.ts for config
-        Unocss(),
-
-        Markdown({
-          wrapperClasses: 'prose prose-sm m-auto text-left',
-          headEnabled: true,
-          markdownItSetup(md) {
-            // https://prismjs.com/
-            md.use(Shiki, {
-              theme: {
-                light: 'vitesse-light',
-                dark: 'vitesse-dark',
-              },
-            })
-            md.use(LinkAttributes, {
-              matcher: (link: string) => /^https?:\/\//.test(link),
-              attrs: {
-                target: '_blank',
-                rel: 'noopener',
-              },
-            })
-          },
-        }),
-
-        VitePWA({
-          registerType: 'autoUpdate',
-          includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
-          manifest: {
-            name: 'Vitesse',
-            short_name: 'Vitesse',
-            theme_color: '#ffffff',
-            icons: [
-              {
-                src: '/pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png',
-              },
-              {
-                src: '/pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-              },
-              {
-                src: '/pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any maskable',
-              },
-            ],
-          },
-        }),
-
-        VueI18n({
-          runtimeOnly: true,
-          compositionOnly: true,
-          include: [path.resolve(__dirname, 'locales/**')],
-        }),
-
-        Inspect(),
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
       ],
-
-      test: {
-        include: ['test/**/*.test.ts'],
-        environment: 'jsdom',
-        deps: {
-          inline: ['@vue', '@vueuse', 'vue-demi'],
-        },
-      },
-
-      ssgOptions: {
-        script: 'async',
-        formatting: 'minify',
-        onFinished() { generateSitemap() },
-      },
-
-      ssr: {
-        // TODO: workaround until they support native ESM
-        noExternal: ['workbox-window', /vue-i18n/],
-      },
-
-    }
-  }
-  else {
-    //  This is the production path
-    return {
-      resolve: {
-        alias: {
-          '~/': `${path.resolve(__dirname, 'src')}/`,
-        },
-      },
-
-      // base: './',
-
-      plugins: [
-        Preview(),
-
-        Vue({
-          include: [/\.vue$/, /\.md$/],
-          reactivityTransform: true,
-        }),
-
-        Pages({
-          extensions: ['vue', 'md'],
-        }),
-
-        Layouts(),
-
-        AutoImport({
-          imports: [
-            'vue',
-            'vue-router',
-            'vue-i18n',
-            'vue/macros',
-            '@vueuse/head',
-            '@vueuse/core',
-          ],
-          dts: 'src/auto-imports.d.ts',
-          dirs: [
-            'src/composables',
-            'src/store',
-          ],
-          vueTemplate: true,
-        }),
-
-        Components({
-          // allow auto load markdown components under `./src/components/`
-          extensions: ['vue', 'md'],
-          // allow auto import and register components used in markdown
-          include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-          dts: 'src/components.d.ts',
-        }),
-
-        // see unocss.config.ts for config
-        Unocss(),
-
-        Markdown({
-          wrapperClasses: 'prose prose-sm m-auto text-left',
-          headEnabled: true,
-          markdownItSetup(md) {
-            // https://prismjs.com/
-            md.use(Shiki, {
-              theme: {
-                light: 'vitesse-light',
-                dark: 'vitesse-dark',
-              },
-            })
-            md.use(LinkAttributes, {
-              matcher: (link: string) => /^https?:\/\//.test(link),
-              attrs: {
-                target: '_blank',
-                rel: 'noopener',
-              },
-            })
-          },
-        }),
-
-        VitePWA({
-          registerType: 'autoUpdate',
-          includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
-          manifest: {
-            name: 'Vitesse',
-            short_name: 'Vitesse',
-            theme_color: '#ffffff',
-            icons: [
-              {
-                src: '/pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png',
-              },
-              {
-                src: '/pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-              },
-              {
-                src: '/pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any maskable',
-              },
-            ],
-          },
-        }),
-
-        VueI18n({
-          runtimeOnly: true,
-          compositionOnly: true,
-          include: [path.resolve(__dirname, 'locales/**')],
-        }),
-
-        Inspect(),
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        'src/store',
       ],
+      vueTemplate: true,
+    }),
 
-      test: {
-        include: ['test/**/*.test.ts'],
-        environment: 'jsdom',
-        deps: {
-          inline: ['@vue', '@vueuse', 'vue-demi'],
-        },
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+
+    // https://github.com/antfu/vite-plugin-vue-markdown
+    // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
+    Markdown({
+      wrapperClasses: 'prose prose-sm m-auto text-left',
+      headEnabled: true,
+      markdownItSetup(md) {
+        // https://prismjs.com/
+        md.use(Shiki, {
+          theme: {
+            light: 'vitesse-light',
+            dark: 'vitesse-dark',
+          },
+        })
+        md.use(LinkAttributes, {
+          matcher: (link: string) => /^https?:\/\//.test(link),
+          attrs: {
+            target: '_blank',
+            rel: 'noopener',
+          },
+        })
       },
+    }),
 
-      ssgOptions: {
-        script: 'async',
-        formatting: 'minify',
-        onFinished() { generateSitemap() },
+    // https://github.com/antfu/vite-plugin-pwa
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: 'Vitesse',
+        short_name: 'Vitesse',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
       },
+    }),
 
-      ssr: {
-        // TODO: workaround until they support native ESM
-        noExternal: ['workbox-window', /vue-i18n/],
-      },
+    // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
+    VueI18n({
+      runtimeOnly: true,
+      compositionOnly: true,
+      include: [path.resolve(__dirname, 'locales/**')],
+    }),
 
-    }
-  }
+    // https://github.com/antfu/vite-plugin-inspect
+    // Visit http://localhost:3333/__inspect/ to see the inspector
+    Inspect(),
+  ],
+
+  // https://github.com/vitest-dev/vitest
+  test: {
+    include: ['test/**/*.test.ts'],
+    environment: 'jsdom',
+    deps: {
+      inline: ['@vue', '@vueuse', 'vue-demi'],
+    },
+  },
+
+  // https://github.com/antfu/vite-ssg
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
+    onFinished() { generateSitemap() },
+  },
+
+  ssr: {
+    // TODO: workaround until they support native ESM
+    noExternal: ['workbox-window', /vue-i18n/],
+  },
 })
