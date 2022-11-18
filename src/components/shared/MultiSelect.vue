379da04bbe3cf11258a4ef2b1c95b36853e8be5f
Sam Tran <samtran0331@gmail.com>
2022-11-18 12:48:32 -0500

    clean up and add shared components

---
+++ src/components/shared/MultiSelect.vue
@@ -0,0 +1,35 @@
+<template>
+	<select
+		v-model="selectedItems"
+		:size="props.listSize"
+		class="form-control"
+		multiple
+		@change="handleSelectionChange"
+	>
+		<option
+			v-for="option in props.options"
+			:key="option.Code"
+			:value="option.Code"
+		>
+			{{ option.Description }}
+		</option>
+	</select>
+</template>
+<script setup lang="ts">
+
+const props = defineProps<{
+    options: []
+    listSize: string
+    modelValue: []
+}>()
+const selectedItems = ref([])
+const emit = defineEmits(['selectionChanged'])
+const handleSelectionChange = (e) => {
+  emit('selectionChanged', selectedItems.value)
+}
+watch(() => props.modelValue, (oldVal, newVal) => {
+	selectedItems.value = oldVal
+    })
+</script>
+<style scoped>
+</style>
---
+++ src/components/shared/WvInput.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="input-group">
+		<label
+			v-if="labelText"
+			for="inputTextBox"
+			class="form-label"
+		>{{ labelText }}</label>
+		<input
+			id="inputTextBox"
+			:value="modelValue"
+			v-bind="$attrs"
+			class="form-control"
+			aria-describedby="smallHelp"
+		>
+		<small
+			v-if="helpText"
+			id="inputHelp"
+			class="form-text text-muted"
+		>{{ helpText }}</small>
+	</div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+	labelText: String
+	placeholderText: String | undefined
+	helpText: String | undefined
+    modelValue: String | undefined
+}>()
+const emit = defineEmits(['update:modelValue'])
+</script>
+<style scoped>
+</style>
