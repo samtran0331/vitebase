91ef8243266552cba86ab0eff56d0034546690e1
Sam Tran <samtran0331@gmail.com>
2022-11-18 11:27:06 -0500

    Everything is working.

--- src/pages/desktop/calendar/index.vue
+++ src/pages/desktop/calendar/index.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { Button } from '@progress/kendo-vue-buttons'
+import { Dialog, DialogActionsBar, Window } from "@progress/kendo-vue-dialogs"
+
 const user = useUserStore()
 const name = $ref(user.savedName)
 
@@ -9,40 +12,100 @@ const go = () => {
 }
 
 const { t } = useI18n()
+const visibleDialog = ref(false)
+function myClick(msg: string) {
+	alert(`I clicked a button:\n${msg}`)
+}
+function toggleDialog() {
+	visibleDialog.value = !visibleDialog.value
+}
+function dialogClick(msg: string) {
+	alert(msg)
+	toggleDialog()
+}
 </script>
 
 <template>
 	<div>
-		<h3>THIS IS THE INDEX.VUE PAGE IN THE CALENDAR FOLDER!</h3>
-		<input
-			id="input"
-			v-model="name"
-			:placeholder="t('intro.whats-your-name')"
-			:aria-label="t('intro.whats-your-name')"
-			type="text"
-			autocomplete="false"
-			p="x4 y2"
-			w="250px"
-			text="center"
-			bg="transparent"
-			border="~ rounded gray-200 dark:gray-700"
-			outline="none active:none"
-			@keydown.enter="go"
-		>
-		<label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-		<div>
-			<button
-				btn
-				m-3
-				text-sm
-				:disabled="!name"
-				@click="go"
+		<h3>THIS IS THE INDEX.VUE PAGE IN THE CALENDAR FOLDER!!!!</h3>
+		<div id="kendo-controls" style="margin: 25px;">
+			<p style="padding: 15px;">
+				<Button
+					theme-color="primary"
+					@click="myClick('hey')"
+				>
+					Test Button!
+				</Button>&nbsp;&nbsp;
+				<Button
+					icon="folder"
+					theme-color="primary"
+					@click="myClick('hi')"
+				>
+					Test Button!
+				</Button>&nbsp;&nbsp;
+				<Button
+					icon="folder"
+					theme-color="primary"
+					@click="myClick('ho')"
+				></Button>
+			</p>
+			<div>
+				<Button @click="toggleDialog">
+					Open Dialog
+				</Button>
+				<Dialog
+					v-if="visibleDialog"
+					title="Please confirm"
+					@close="toggleDialog"
+				>
+					<p :style="{ margin: '25px', textAlign: 'center' }">
+						Are you sure you want to continue?
+					</p>
+					<DialogActionsBar>
+						<Button @click="dialogClick('No!')">
+							No
+						</Button>
+						<Button @click="dialogClick('Yes!')">
+							Yes
+						</Button>
+					</DialogActionsBar>
+				</Dialog>
+			</div>
+		</div>
+		<div id="name-test">
+			<input
+				id="input"
+				v-model="name"
+				:placeholder="t('intro.whats-your-name')"
+				:aria-label="t('intro.whats-your-name')"
+				type="text"
+				autocomplete="false"
+				p="x4 y2"
+				w="250px"
+				text="center"
+				bg="transparent"
+				border="~ rounded gray-200 dark:gray-700"
+				outline="none active:none"
+				@keydown.enter="go"
 			>
-				{{ t('button.go') }}
-			</button>
+			<label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
+			<div>
+				<button
+					btn
+					m-3
+					text-sm
+					:disabled="!name"
+					@click="go"
+				>
+					{{ t('button.go') }}
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
+
+<style scoped>
+</style>
 
 <route lang="yaml">
 meta:
