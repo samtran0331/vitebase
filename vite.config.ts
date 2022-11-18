52a7f64fc16c722827f71369d9b7d112f01bf2f2
Sam Tran <samtran0331@gmail.com>
2022-11-18 09:06:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,144 +15,294 @@ import LinkAttributes from 'markdown-it-link-attributes'
 import Unocss from 'unocss/vite'
 import Shiki from 'markdown-it-shiki'
 
-export default defineConfig({
-  base: '/webvantage/vue/',
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    // serve is dev environment with vite
+    return {
+      // base: '/webvantage/vue/',
 
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-
-  plugins: [
-    Preview(),
-
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-      reactivityTransform: true,
-    }),
-
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages({
-      extensions: ['vue', 'md'],
-    }),
-
-    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
-
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        'vue/macros',
-        '@vueuse/head',
-        '@vueuse/core',
-      ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: [
-        'src/composables',
-        'src/store',
-      ],
-      vueTemplate: true,
-    }),
-
-    // https://github.com/antfu/unplugin-vue-components
-    Components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'src/components.d.ts',
-    }),
-
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    Unocss(),
-
-    // https://github.com/antfu/vite-plugin-vue-markdown
-    // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
-    Markdown({
-      wrapperClasses: 'prose prose-sm m-auto text-left',
-      headEnabled: true,
-      markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Shiki, {
-          theme: {
-            light: 'vitesse-light',
-            dark: 'vitesse-dark',
-          },
-        })
-        md.use(LinkAttributes, {
-          matcher: (link: string) => /^https?:\/\//.test(link),
-          attrs: {
-            target: '_blank',
-            rel: 'noopener',
-          },
-        })
+      resolve: {
+        alias: {
+          '~/': `${path.resolve(__dirname, 'src')}/`,
+        },
       },
-    }),
 
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
-      manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+      plugins: [
+        Preview(),
+
+        Vue({
+          include: [/\.vue$/, /\.md$/],
+          reactivityTransform: true,
+        }),
+
+        // https://github.com/hannoeru/vite-plugin-pages
+        Pages({
+          extensions: ['vue', 'md'],
+        }),
+
+        // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+        Layouts(),
+
+        // https://github.com/antfu/unplugin-auto-import
+        AutoImport({
+          imports: [
+            'vue',
+            'vue-router',
+            'vue-i18n',
+            'vue/macros',
+            '@vueuse/head',
+            '@vueuse/core',
+          ],
+          dts: 'src/auto-imports.d.ts',
+          dirs: [
+            'src/composables',
+            'src/store',
+          ],
+          vueTemplate: true,
+        }),
+
+        // https://github.com/antfu/unplugin-vue-components
+        Components({
+          // allow auto load markdown components under `./src/components/`
+          extensions: ['vue', 'md'],
+          // allow auto import and register components used in markdown
+          include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+          dts: 'src/components.d.ts',
+        }),
+
+        // https://github.com/antfu/unocss
+        // see unocss.config.ts for config
+        Unocss(),
+
+        // https://github.com/antfu/vite-plugin-vue-markdown
+        // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
+        Markdown({
+          wrapperClasses: 'prose prose-sm m-auto text-left',
+          headEnabled: true,
+          markdownItSetup(md) {
+            // https://prismjs.com/
+            md.use(Shiki, {
+              theme: {
+                light: 'vitesse-light',
+                dark: 'vitesse-dark',
+              },
+            })
+            md.use(LinkAttributes, {
+              matcher: (link: string) => /^https?:\/\//.test(link),
+              attrs: {
+                target: '_blank',
+                rel: 'noopener',
+              },
+            })
           },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+        }),
+
+        // https://github.com/antfu/vite-plugin-pwa
+        VitePWA({
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+          manifest: {
+            name: 'Vitesse',
+            short_name: 'Vitesse',
+            theme_color: '#ffffff',
+            icons: [
+              {
+                src: '/pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: '/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+              {
+                src: '/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable',
+              },
+            ],
           },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
+        }),
+
+        // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
+        VueI18n({
+          runtimeOnly: true,
+          compositionOnly: true,
+          include: [path.resolve(__dirname, 'locales/**')],
+        }),
+
+        // https://github.com/antfu/vite-plugin-inspect
+        // Visit http://localhost:3333/__inspect/ to see the inspector
+        Inspect(),
+      ],
+
+      // https://github.com/vitest-dev/vitest
+      test: {
+        include: ['test/**/*.test.ts'],
+        environment: 'jsdom',
+        deps: {
+          inline: ['@vue', '@vueuse', 'vue-demi'],
+        },
       },
-    }),
 
-    // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
-    VueI18n({
-      runtimeOnly: true,
-      compositionOnly: true,
-      include: [path.resolve(__dirname, 'locales/**')],
-    }),
+      // https://github.com/antfu/vite-ssg
+      ssgOptions: {
+        script: 'async',
+        formatting: 'minify',
+        onFinished() { generateSitemap() },
+      },
 
-    // https://github.com/antfu/vite-plugin-inspect
-    // Visit http://localhost:3333/__inspect/ to see the inspector
-    Inspect(),
-  ],
+      ssr: {
+        // TODO: workaround until they support native ESM
+        noExternal: ['workbox-window', /vue-i18n/],
+      },
 
-  // https://github.com/vitest-dev/vitest
-  test: {
-    include: ['test/**/*.test.ts'],
-    environment: 'jsdom',
-    deps: {
-      inline: ['@vue', '@vueuse', 'vue-demi'],
-    },
-  },
+    }
+  }
+  else
+  {
+    return {
+      base: '/webvantage/vue/',
 
-  // https://github.com/antfu/vite-ssg
-  ssgOptions: {
-    script: 'async',
-    formatting: 'minify',
-    onFinished() { generateSitemap() },
-  },
+      resolve: {
+        alias: {
+          '~/': `${path.resolve(__dirname, 'src')}/`,
+        },
+      },
 
-  ssr: {
-    // TODO: workaround until they support native ESM
-    noExternal: ['workbox-window', /vue-i18n/],
-  },
+      plugins: [
+        Preview(),
+
+        Vue({
+          include: [/\.vue$/, /\.md$/],
+          reactivityTransform: true,
+        }),
+
+        // https://github.com/hannoeru/vite-plugin-pages
+        Pages({
+          extensions: ['vue', 'md'],
+        }),
+
+        // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+        Layouts(),
+
+        // https://github.com/antfu/unplugin-auto-import
+        AutoImport({
+          imports: [
+            'vue',
+            'vue-router',
+            'vue-i18n',
+            'vue/macros',
+            '@vueuse/head',
+            '@vueuse/core',
+          ],
+          dts: 'src/auto-imports.d.ts',
+          dirs: [
+            'src/composables',
+            'src/store',
+          ],
+          vueTemplate: true,
+        }),
+
+        // https://github.com/antfu/unplugin-vue-components
+        Components({
+          // allow auto load markdown components under `./src/components/`
+          extensions: ['vue', 'md'],
+          // allow auto import and register components used in markdown
+          include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+          dts: 'src/components.d.ts',
+        }),
+
+        // https://github.com/antfu/unocss
+        // see unocss.config.ts for config
+        Unocss(),
+
+        // https://github.com/antfu/vite-plugin-vue-markdown
+        // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
+        Markdown({
+          wrapperClasses: 'prose prose-sm m-auto text-left',
+          headEnabled: true,
+          markdownItSetup(md) {
+            // https://prismjs.com/
+            md.use(Shiki, {
+              theme: {
+                light: 'vitesse-light',
+                dark: 'vitesse-dark',
+              },
+            })
+            md.use(LinkAttributes, {
+              matcher: (link: string) => /^https?:\/\//.test(link),
+              attrs: {
+                target: '_blank',
+                rel: 'noopener',
+              },
+            })
+          },
+        }),
+
+        // https://github.com/antfu/vite-plugin-pwa
+        VitePWA({
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+          manifest: {
+            name: 'Vitesse',
+            short_name: 'Vitesse',
+            theme_color: '#ffffff',
+            icons: [
+              {
+                src: '/pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: '/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+              {
+                src: '/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable',
+              },
+            ],
+          },
+        }),
+
+        // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
+        VueI18n({
+          runtimeOnly: true,
+          compositionOnly: true,
+          include: [path.resolve(__dirname, 'locales/**')],
+        }),
+
+        // https://github.com/antfu/vite-plugin-inspect
+        // Visit http://localhost:3333/__inspect/ to see the inspector
+        Inspect(),
+      ],
+
+      // https://github.com/vitest-dev/vitest
+      test: {
+        include: ['test/**/*.test.ts'],
+        environment: 'jsdom',
+        deps: {
+          inline: ['@vue', '@vueuse', 'vue-demi'],
+        },
+      },
+
+      // https://github.com/antfu/vite-ssg
+      ssgOptions: {
+        script: 'async',
+        formatting: 'minify',
+        onFinished() { generateSitemap() },
+      },
+
+      ssr: {
+        // TODO: workaround until they support native ESM
+        noExternal: ['workbox-window', /vue-i18n/],
+      },
+        }
+  }
 })
